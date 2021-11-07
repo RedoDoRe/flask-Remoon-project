595bcb05778227da67ba8c1e21a687b3ba94b8cb
modunn <thasv0279@gmail.com>
2021-11-08 02:18:50 +0700

    mundo

--- index.py
+++ index.py
@@ -1,20 +1,39 @@
-from flask import Flask, request, render_template,url_for
+from flask import (Flask, 
+				request,
+				 render_template,
+				 url_for ,
+				 redirect)
 
 
 app = Flask(__name__)
 
+
+
+
+
 @app.route('/home')
 def home():
-	return '<p>Hello Word</p>'
+	return render_template('layout.html')
 
-@app.route('/')
-@app.route('/signin')
-def login():
-	return render_template('login.html')
+
+
+@app.route('/',methods=['GET','POST'])
+@app.route('/signin',methods=['GET','POST'])
+def signin():
+	message =''
+	if request.method =='POST':
+		email = request.form['email']
+		password = request.form['password']
+		if  email == 'admin' and password =='admin':		
+			return redirect(url_for('home'))
+		else:
+			message= 'Sai tên tài khoản hoặc mật khẩu'
+			
+	return render_template('signin.html',title ='sign in',message=message)
 
 @app.route('/signup')
-def register():
-	return render_template('register.html')
+def signup():
+	return render_template('signup.html',title ='sign up')
 
 
 if __name__ == '__main__':
